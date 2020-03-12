77ed0f461d545bc0a335f851b11b83b222e94bfd
alaaamr19 <54320354+alaaamr19@users.noreply.github.com>
2020-03-12 13:54:02 +0200

    modify DisplayOrder file

--- Model/order.class.php
+++ Model/order.class.php
@@ -6,11 +6,10 @@
 
         public function displayOrders($user_id){
             global $db;
-            $result=mysqli_query($db,"SELECT clients.user_name,orders.date ,orders.state,
+            $result=mysqli_query($db,"SELECT orders.date ,orders.state,
             products.price,order_product.product_amount
             , orders.total_price ,orders.order_notes ,products.product_name,orders.order_id
-            from orders inner join clients on clients.user_id=orders.user_id
-            INNER JOIN order_product on order_product.order_id=orders.order_id 
+            from orders INNER JOIN order_product on order_product.order_id=orders.order_id 
             INNER JOIN products on order_product.product_id=products.product_id 
             where orders.user_id={$user_id}");
             return $result;
