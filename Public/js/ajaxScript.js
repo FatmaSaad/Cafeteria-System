d6404250a0c7db1c4d826eb2f5aa06e8f4cc918c
Mahmoud <mahmoedmohamed19@gmail.com>
2020-03-16 16:32:15 +0200

    ajax

--- Public/js/ajaxScript.js
+++ Public/js/ajaxScript.js
@@ -6,43 +6,42 @@ function getUpdateProduct(button) {
         url: "http://localhost/Cafeteria-System/Controller/productEdit.php",
         // url:"productEdit.php",
         method: "GET",
-        data:{"id":id},
+        data: { "id": id },
         datatype: "json",
         success: function (response) {
             // console.log(response);
             var product = JSON.parse(response);
-            console.log(product);
+            // console.log(product);
             $("#productId").val(product.product_id);
             $("#productName").val(product.product_name);
             $("#price").val(product.price);
             $("#selected").text(product.category);
-            $("#pictureShow").attr('src',`../uploads/${product.image}`);            
-
+            $("#pictureShow").attr('src', `../uploads/${product.image}`);
         }
     });
 }
 $("#updateProduct").submit(function (e) {
-    e.preventDefault();  
-    console.log( $(this).serialize());
+    e.preventDefault();
     $.ajax({
         type: $(this).attr('method'),
         url: $(this).attr('action'),
-        data: $(this).serialize(),
-        datatype:"json",
+        // data: $(this).serialize(),
+        data: new FormData(this),
+        contentType: false,
+        cache: false,
+        processData:false,
+        datatype: "json",
         success: function (msg) {
-            //pass ;
-            // var newmsg = JSON.parse(msg);
-            console.log(currentRow.parents("tr").find("img"));
+            console.log(msg);
             
-           console.log($("#productName").val());
             currentRow.parents("tr").find("td:eq(1)").text($("#productName").val());
+            currentRow.parents("tr").find("td:eq(2)").text($("#category option:selected").val());
             currentRow.parents("tr").find("td:eq(3)").text($("#price").val());
-            currentRow.parents("tr").find("td:eq(2)").text($("#category").val());
             // currentRow.parents("tr").find("img)").attr('src',`../uploads/${"#image"}`);
             $('#myModal').modal('toggle');
         },
         error: function () {
-            //alert("failure");
+            alert("failure");
         }
     });
 });
--- Views/adminHeader.php
+++ Views/adminHeader.php
@@ -1,12 +1,12 @@
-<?php 
-    require_once("../config.php");
+<?php
+require_once("../config.php");
 
-    // if (isset($_SESSION['role'])){
-    //     if ($_SESSION['role'] != 'admin')
-    //     header("Location:./DisplayOrders.php");
-    // }else{
-    //     header("Location:./");
-    // }
+// if (isset($_SESSION['role'])){
+//     if ($_SESSION['role'] != 'admin')
+//     header("Location:./DisplayOrders.php");
+// }else{
+//     header("Location:./");
+// }
 ?>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -22,14 +22,18 @@
             <li class="nav-item active">
                 <a class="nav-link" href="DisplayOrdersAdmin.php">Home <span class="sr-only">(current)</span></a>
             </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="addProduct.php">Add Product</a>
+            </li>
             <li class="nav-item">
                 <a class="nav-link" href="listProduct.php">Products</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="usersList.php">Users</a>
+                <a class="nav-link" href="addUser.php">Add User</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="addUser.php">Add User</a>
+                <a class="nav-link" href="usersList.php">Users</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="addOrderManually.php">Manual Order</a>
